3878b75d814f4c62e49c707b26f36e075365a981
mohamad-dev12 <programminglearn360@gmail.com>
2024-11-21 23:01:23 +0100

    refactor

--- scripts/template.js
+++ scripts/template.js
@@ -1,36 +1,35 @@
 function sidebarShow() {
-  return `<a href="../pages/summary.html"><img class="logo" src="../assets/img/logo-white.png" alt="logo"/></a>
-          <div class="navigation">
-              <div id="summary">
-                  <a href="../pages/summary.html" class="d_flex_c g_12 w-100">     
-                      <img src="../assets/icon/summary.png" alt="summary icon"/>    
-                      <span>Summary</span>
-                  </a>
-               </div>   
-              <div id="task">
-                  <a href="../pages/add_task.html" class="d_flex_c g_12 w-100">     
-                      <img src="../assets/icon/addTask.png" alt="task icon"/>    
-                      <span>Add Task</span>
-                  </a>
-              </div> 
-              <div id="board">
-                  <a href="../pages/board.html" class="d_flex_c g_12 w-100">     
-                      <img src="../assets/icon/board.png" alt="board icon"/>    
-                      <span>Board</span>
-                  </a>
-              </div>   
-              <div id="contacts">
-                  <a href="../pages/contacts.html" class="d_flex_c g_12 w-100">     
-                      <img src="../assets/icon/contacts.png" alt="contacts icon"/>    
-                      <span>Contacts</span>
-                  </a>
-              </div> 
-          </div>
-          <div class="privacy-container">
-        <a href="privacy_policy.html">Privacy Policy</a>
-        <a href="legal_notice.html">Legal notice</a>
+  return /*html*/ `<a href="../pages/summary.html"><img class="logo" src="../assets/img/logo-white.png" alt="logo" /></a>
+    <div class="navigation">
+      <div id="summary">
+        <a href="../pages/summary.html" class="d_flex_c g_12 w-100">
+          <img src="../assets/icon/summary.png" alt="summary icon" />
+          <span>Summary</span>
+        </a>
       </div>
-          `;
+      <div id="task">
+        <a href="../pages/add_task.html" class="d_flex_c g_12 w-100">
+          <img src="../assets/icon/addTask.png" alt="task icon" />
+          <span>Add Task</span>
+        </a>
+      </div>
+      <div id="board">
+        <a href="../pages/board.html" class="d_flex_c g_12 w-100">
+          <img src="../assets/icon/board.png" alt="board icon" />
+          <span>Board</span>
+        </a>
+      </div>
+      <div id="contacts">
+        <a href="../pages/contacts.html" class="d_flex_c g_12 w-100">
+          <img src="../assets/icon/contacts.png" alt="contacts icon" />
+          <span>Contacts</span>
+        </a>
+      </div>
+    </div>
+    <div class="privacy-container">
+      <a href="privacy_policy.html">Privacy Policy</a>
+      <a href="legal_notice.html">Legal notice</a>
+    </div> `;
 }
 
 function getListTemplate(value, option) {
@@ -41,7 +40,7 @@ function getAddTaskTemplate(isEditMode, task, date, subTasks) {
   return `<div class="d_flex_column" data-modal>
               <div class="d_flex_c main-div">
                 <div class="media-w-300">
-                    <h1 style="margin-bottom: 12px;" class=" ${isEditMode ? 'd_none' : ''}">Add Task</h1>
+                    <h1  class=" ${isEditMode ? 'd_none' : ''}">Add Task</h1>
                     <div>
                         <div class="flex">
                             <span>Title<span class="color-red">*</span></span>
