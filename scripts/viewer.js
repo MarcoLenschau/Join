73456059cdbdcde719935139e47e9bb7e9e474b6
Marco Lenschau <contact@marco-lenschau.de>
2025-06-08 05:50:40 +0200

    doc: Comment last not commentatet funktion

--- scripts/viewer.js
+++ scripts/viewer.js
@@ -1,3 +1,8 @@
+/**
+ * Opens an image viewer for the specified container element.
+ * 
+ * @param {string} element - The ID of the container holding the images.
+ */
 function bigPicture(element) {
     const imageContainer = document.getElementById(element);
     const viewer = new Viewer(imageContainer, {
