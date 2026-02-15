a242dcf913f440fef93c1484a2cc0747c4e7ab29
MarcoLenschau <contact@marco-lenschau.de>
2026-02-15 20:00:21 +0100

    fix: Delete reference error

--- scripts/contact.js
+++ scripts/contact.js
@@ -357,7 +357,7 @@ function userImgDefine(numberOfContact, dialog="") {
  * @param {Object} dialog - A dialog object (used for handling UI updates or confirmations).
  */
 function createUserImage(numberOfContact, dialog) {
-  const imagepicker = createImagePicker();
+  const imagepicker = createImagePicker(numberOfContact);
   imagepicker.addEventListener("change", () => {
     const image = imagepicker.files;
     document.querySelector(".person-icon").classList.remove("person-icon-without-picture");
@@ -379,7 +379,7 @@ function createUserImage(numberOfContact, dialog) {
  * @param {number|null} numberOfContact - The ID or index of the contact. If null, the default image picker is used.
  * @returns {HTMLInputElement} The image input element from the DOM.
  */
-function createImagePicker() {
+function createImagePicker(numberOfContact) {
   let imagepicker;
   if (numberOfContact === null) {
     imagepicker = document.getElementById("imagepicker");
